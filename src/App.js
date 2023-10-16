ec44f0d44e85a6e80370c01ebbb83e1dfe00c6e0
elenareach-developer <elenareach@gmail.com>
2023-10-16 20:13:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,214 @@
-import logo from './logo.svg';
-import './App.css';
+import
+{
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useState } from "react";
+import deved from "./images/dev-ed-wave.png";
+import code from "./images/code.png";
+import design from "./images/design.png";
+import consulting from "./images/consulting.png";
+import web1 from "./images/web1.png";
+import web2 from "./images/web2.png";
+import web3 from "./images/web3.png";
+import web4 from "./images/web4.png";
+import web5 from "./images/web5.png";
+import web6 from "./images/web6.png";
 
-function App() {
+
+export default function Home ()
+{
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={ darkMode ? "dark" : "" }>
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">React With Tailwindcss</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={ () => setDarkMode(!darkMode) }
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+              Elene Reach
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Developer and designer.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <AiFillTwitterCircle />
+              <AiFillLinkedin />
+              <AiFillYoutube />
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <img src={ deved } layout="fill" objectFit="cover" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <img src={ design } width={ 100 } height={ 100 } />
+              <h3 className="text-lg font-medium pt-8 pb-2  ">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <img className="left-6" src={ code } width={ 100 } height={ 100 } />
+              <h3 className="text-lg font-medium pt-8 pb-2 ">
+                Code your dream project
+              </h3>
+              <p className="py-2">
+                lorem ipsum dolor sit amet, adip lorem ipsum dolor sit amet dolar ipsum
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <img src={ consulting } width={ 100 } height={ 100 } />
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
+              <p className="py-2">
+                lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+          </div>
+        </section>
+        <section className="py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <img
+                className="rounded-lg object-cover"
+                width={ "100%" }
+                height={ "100%" }
+                layout="responsive"
+                src={ web1 }
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={ "100%" }
+                height={ "100%" }
+                layout="responsive"
+                src={ web2 }
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={ "100%" }
+                height={ "100%" }
+                layout="responsive"
+                src={ web3 }
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={ "100%" }
+                height={ "100%" }
+                layout="responsive"
+                src={ web4 }
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={ "100%" }
+                height={ "100%" }
+                layout="responsive"
+                src={ web5 }
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={ "100%" }
+                height={ "100%" }
+                layout="responsive"
+                src={ web6 }
+              />
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
 
-export default App;
+
+
+// import './App.css';
+
+// function App ()
+// {
+//   return (
+//     <div>
+//       <h1 className="text-6xl font-bold underline">
+//         Hello World!
+//       </h1>
+//     </div>
+//   );
+// }
+// export default App;
